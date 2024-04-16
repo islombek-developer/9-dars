8fac8b8d6575ef9a288688949dc122c37e9c04dd
islombek-developer <164884102+islombek-developer@users.noreply.github.com>
2024-04-16 20:37:43 +0300

    masala

--- salom.py
+++ salom.py
@@ -1,97 +1,34 @@
-import time
 import requests
-start=time.perf_counter()
-img_urls = [
-    'https://images.unsplash.com/photo-1516117172878-fd2c41f4a759',
-    'https://images.unsplash.com/photo-1532009324734-20a7a5813719',
-    'https://images.unsplash.com/photo-1524429656589-6633a470097c',
-    'https://images.unsplash.com/photo-1530224264768-7ff8c1789d79',
-    'https://images.unsplash.com/photo-1564135624576-c5c88640f235',
-    'https://images.unsplash.com/photo-1541698444083-023c97d3f4b6',
-    'https://images.unsplash.com/photo-1522364723953-452d3431c267',
-    'https://images.unsplash.com/photo-1513938709626-033611b8cc03',
-    'https://images.unsplash.com/photo-1507143550189-fed454f93097',
-    'https://images.unsplash.com/photo-1493976040374-85c8e12f0c0e',
-    'https://images.unsplash.com/photo-1504198453319-5ce911bafcde',
-    'https://images.unsplash.com/photo-1530122037265-a5f1f91d3b99',
-    'https://images.unsplash.com/photo-1516972810927-80185027ca84',
-    'https://images.unsplash.com/photo-1550439062-609e1531270e',
-    'https://images.unsplash.com/photo-1549692520-acc6669e2f0c'
+import threading
+import time
+
+start = time.perf_counter()
+
+text_urls = [
+    'https://uz.wikipedia.org/wiki/Arslon',
+    'https://bank.uz/currency#RUB',
+    'https://www.bbc.com/news'
 ]
-for imeg in img_urls:
-    img_connect=requests.get(imeg).content
-    img_name=f"{imeg.split('/')[-1]}.jpg"
-    with open(img_name,"wb") as img_file:
-        img_file.write(img_connect)
-        print(f"{img_name} dowload")
 
-end=time.perf_counter()
-print(f"{end-start} tugadi")
+def write_text(url):
+    try:
+        response = requests.get(url)
+        text_content = response.text
+        file_name = f"{url.split('/')[-1]}.txt"
+        with open(file_name, "w", encoding="utf-8") as text_file:
+            text_file.write(text_content)
+            print(f"{file_name} kochirildi")
+    except Exception as e:
+        print(f"Error {url}: {e}")
 
+threads = []
+for url in text_urls:
+    thread = threading.Thread(target=write_text, args=[url])
+    thread.start()
+    threads.append(thread)
 
+for thread in threads:
+    thread.join()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# import threading
-
-# start=time.perf_counter()
-# def foo(num):
-#     print(f"Hello word {num} seconds ...")
-#     print("tugadi")
-# threads=[]
-# for i in range(15):
-#     t=threading.Thread(target=foo, args=[5])
-#     t.start()
-#     threads.append(t)
-# for i in threads:
-#     i.join()
-# end=time.perf_counter()
-# print(f"salom {start-end}")
-
-# import time
-# import threading
-# start=time.perf_counter()
-# def foo():
-#     print("1 second otdi")
-#     time.sleep(0.1)
-#     print("tugadi")
-# threads=[]
-# for i in range(15):
-#     t=threading.Thread(target=foo)
-#     t.start()
-#     threads.append(t)
-# for i in threads:
-#     i.join()
-
-# end=time.perf_counter()
-# print(f"tugatildi {end-start} shu vaqtda")
-
-# import time
-# import threading
-# start=time.perf_counter()
-# def foo(second,num):
-#     print(f"{num} second otdi")
-#     time.sleep(second)
-#     print("tugadi")
-# threads=[]
-# for i in range(10):
-#     t=threading.Thread(target=foo, args=[1,5])
-#     t.start()
-#     threads.append(t)
-# for i in threads:
-#     i.join()
-
-# end=time.perf_counter()
-# print(f"tugatildi {end-start} shu vaqtda")
+end = time.perf_counter()
+print(f"{end - start} seconds elapsed")
--- vazifa.py
+++ vazifa.py
@@ -1,29 +1,31 @@
-
+import csv
 import requests
 import threading
 import time
 
 start = time.perf_counter()
 
-img_urls = ["Users\HP\Desktop "]
+img_url = "https://docs.google.com/spreadsheets/d/1vpMsFooIDKq8RJ-9MVbEmjI4NlUCyxThByIi4frhcGQ/export?format=csv"
 
-def img_download(img_url):
+def img_download(url):
     try:
-        img_name = f"{img_url.split()}.txt"
+        response = requests.get(url)
+        img_content = response.text
+        img_name = "data.csv"  
         with open(img_name, "wb") as img_file:
             img_file.write(img_content)
             print(f"{img_name} downloaded")
-    except:
-        with open("img_name", "wb") as img_file:
-            img_file.write("xatolik bor 404")
+
+    except Exception as e:
+        print(f"Error  {url}: {e}")
+
 threads = []
-for image_url in img_urls:
-    th = threading.Thread(target=img_download, args=[image_url])
-    th.start()
-    threads.append(th)
+th = threading.Thread(target=img_download, args=[img_url])
+th.start()
+threads.append(th)
 
 for t in threads:
     t.join()
 
 end = time.perf_counter()
-print(f"{end - start} seconds elapsed")
+print(f"{end - start} second")
